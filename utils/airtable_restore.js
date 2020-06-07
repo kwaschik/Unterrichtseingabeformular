658e6bfcd4669ee33fc2c33636a1a79109f04c99
Kilian Kwaschik <kwaschik@startmail.com>
2020-06-07 23:44:22 +0200

    refactoring of the restore script, it now uses promises

--- utils/airtable_restore.js
+++ utils/airtable_restore.js
@@ -17,97 +17,93 @@ const base = new Airtable({
 }).base(_base);
 
 
-let mapping = {};
-function processRecords(records, stringifyRecord, table, next, offset=0) {
-	if (offset >= records.length) {
-		try { 
-			console.log(`... ${table} fertig`);
-			next(); 
-		} 
-		catch {}
-		finally { return; }
-	}
-	// airtable max. 10 pro request
-	let bckRecords = records.slice(offset, offset+10);
-	base(table).create(bckRecords.map(stringifyRecord), (err, createdRecords) => {
-		if (err) {
-			console.error(`${table}, Fehler: ${err}`);
-			return;
-		}
-		createdRecords.forEach(rec => {
-			// da automatisch neue IDs vergeben werden, muessen wir
-			// die alte (aus dem Backup) und die neue ID verknuepfen
-			let old = bckRecords.find(element => 
-				element.fields[table] === rec.get(table))
-			mapping[table][old.id] = rec.getId();
-		});
-		processRecords(records, stringifyRecord, table, next, offset+10)
-	});
+function createMappingContext() {
+	return Promise.resolve({});
 }
-function restore(table, stringifyRecord, next=null) {
-	return function() {
-		console.log(table);
 
-		mapping[table] = {};
+function restore(table, stringifyRecord, ctx) {
+	let processRecords = (records, stringifyRecord, table, ctx, resolve, reject, offset=0) => {
+		if (offset >= records.length) {
+			console.log(`... ${table} fertig`);
+			return resolve(ctx);
+		}
+		// airtable max. 10 pro request
+		let bckRecords = records.slice(offset, offset+10);
+		base(table).create(bckRecords.map(rec => stringifyRecord(ctx, rec)), (err, createdRecords) => {
+			if (err) return reject(err);
+			createdRecords.forEach(rec => {
+				// da automatisch neue IDs vergeben werden, muessen wir
+				// die alte (aus dem Backup) und die neue ID verknuepfen
+				let old = bckRecords.find(element => 
+					element.fields[table] === rec.get(table))
+				ctx[table][old.id] = rec.getId();
+			});
+			processRecords(records, stringifyRecord, table, ctx, resolve, reject, offset+10)
+		});
+	}
+
+	return new Promise((resolve, reject) => {
+		console.log(table);
+		ctx[table] = {};
 		fs.readFile(`${restorePath}/${table}.json`, "utf8", (err, data) => {
-			if (err) throw err;
+			if (err) return reject(err);
 			let backup_data = JSON.parse(data).records.sort(
 				(rec1, rec2) => rec1.fields.Counter - rec2.fields.Counter);
-			processRecords(backup_data, stringifyRecord, table, next);
+			processRecords(backup_data, stringifyRecord, table, ctx, resolve, reject);
 		});
-	}
+	});
 }
 
-function getFachId(rec) {
+function getFachId(mapping, rec) {
 	try {
 		return [mapping.Fach[rec.fields.Fach[0]]]
 	} catch {
 		return []
 	}
 }
-function getFachbereichId(rec) {
+function getFachbereichId(mapping, rec) {
 	try {
 		return [mapping.Fachbereich[rec.fields.Fachbereich[0]]]
 	} catch {
 		return []
 	}
 }
-function getThemaId(rec) {
+function getThemaId(mapping, rec) {
 	try {
 		return [mapping.Thema[rec.fields.Thema[0]]];
 	} catch {
 		return [];
 	}
 }
-function getThemenbereichId(rec) {
+function getThemenbereichId(mapping, rec) {
 	try {
 		return [mapping.Themenbereich[rec.fields.Themenbereich[0]]];
 	} catch {
 		return [];
 	}
 }
-function getDysfunktionenId(rec) {
+function getDysfunktionenId(mapping, rec) {
 	try {
 		return [mapping.Dysfunktionen[rec.fields.Dysfunktionen[0]]];
 	} catch {
 		return [];
 	}
 }
-function getPathologieId(rec) {
+function getPathologieId(mapping, rec) {
 	try {
 		return [mapping.Pathologie[rec.fields.Pathologie[0]]];
 	} catch {
 		return [];
 	}
 }
-function getDozentId(rec) {
+function getDozentId(mapping, rec) {
 	try {
 		return [mapping.Dozent[rec.fields.Dozent[0]]];
 	} catch {
 		return [];
 	}
 }
-function getJahrgangId(rec) {
+function getJahrgangId(mapping, rec) {
 	try {
 		return [mapping.Jahrgang[rec.fields.Jahrgang[0]]];
 	} catch {
@@ -115,106 +111,109 @@ function getJahrgangId(rec) {
 	}
 }
 
+// TODO mapping context / stringify-funktion in einem Objekt zusammenfuehren?
+function restoreAll() {
+	createMappingContext()
+		.then(ctx => restore("Fach", 
+				(_, rec) => 
+				({
+					"fields":
+					{
+						"Fach": rec.fields.Fach
+					}
+				}), ctx))
+		.then(ctx => restore("Fachbereich", 
+				(mapping, rec) => 
+				({
+					"fields":
+					{
+						"Fachbereich": rec.fields.Fachbereich,
+						"Fach": getFachId(mapping, rec)
+					}
+				}), ctx))
+		.then(ctx => restore("Thema",
+				(mapping, rec) => 
+				({
+					"fields":
+					{
+						"Thema": rec.fields.Thema,
+						"Fachbereich": getFachbereichId(mapping, rec),
+						"Fach": getFachId(mapping, rec)
+					}
+				}), ctx))
+		.then(ctx => restore("Themenbereich",
+				(mapping, rec) => 
+				({
+					"fields":
+					{
+						"Themenbereich": rec.fields.Themenbereich,
+						"Thema": getThemaId(mapping, rec),
+						"Fachbereich": getFachbereichId(mapping, rec),
+						"Fach": getFachId(mapping, rec)
+					}
+				}), ctx))
+		.then(ctx => restore("Dysfunktionen",
+				(mapping, rec) => 
+				({
+					"fields":
+					{
+						"Dysfunktionen": rec.fields.Dysfunktionen,
+						"Themenbereich": getThemenbereichId(mapping, rec),
+						"Thema": getThemaId(mapping, rec),
+						"Fachbereich": getFachbereichId(mapping, rec),
+						"Fach": getFachId(mapping, rec)
+					}
+				}), ctx))
+		.then(ctx => restore("Pathologie",
+				(mapping, rec) => 
+				({
+					"fields":
+					{
+						"Pathologie": rec.fields.Pathologie,
+						"Dysfunktionen": getDysfunktionenId(mapping, rec),
+						"Themenbereich": getThemenbereichId(mapping, rec),
+						"Thema": getThemaId(mapping, rec),
+						"Fachbereich": getFachbereichId(mapping, rec),
+						"Fach": getFachId(mapping, rec)
+					}
+				}), ctx))
+		.then(ctx => restore("Dozent", 
+				(_, rec) => 
+				({
+					"fields":
+					{
+						"Dozent": rec.fields.Dozent
+					}
+				}), ctx))
+		.then(ctx => restore("Jahrgang", 
+				(_, rec) => 
+				({
+					"fields":
+					{
+						"Jahrgang": rec.fields.Jahrgang
+					}
+				}), ctx))
+		.then(ctx => restore("Klassenbuch",
+				(mapping, rec) => 
+				({
+					"fields":
+					{
+						"Name": rec.fields.Name,
+						"Dozent": getDozentId(mapping, rec),
+						"Assistent": rec.fields.Assistent,
+						"Jahrgang": getJahrgangId(mapping, rec),
+						"Datum": rec.fields.Datum,
+						"Notes": rec.fields.Notes,
+						"Fach": getFachId(mapping, rec),
+						"Fachbereich": getFachbereichId(mapping, rec),
+						"Thema": getThemaId(mapping, rec),
+						"Themenbereich": getThemenbereichId(mapping, rec),
+						"Dysfunktionen": getDysfunktionenId(mapping, rec),
+						"Pathologie": getPathologieId(mapping, rec),
+						"Stunden": rec.fields.Stunden
+					}
+				}), ctx))
+		.catch(err => console.error(err))
+};
 
-(function restoreAll() {
-	restore("Fach", 
-		rec => 
-		({
-			"fields":
-			{
-				"Fach": rec.fields.Fach
-			}
-		}),
-	restore("Fachbereich", 
-		rec => 
-		({
-			"fields":
-			{
-				"Fachbereich": rec.fields.Fachbereich,
-				"Fach": getFachId(rec)
-			}
-		}),
-	restore("Thema",
-		rec => 
-		({
-			"fields":
-			{
-				"Thema": rec.fields.Thema,
-				"Fachbereich": getFachbereichId(rec),
-				"Fach": getFachId(rec)
-			}
-		}), 
-	restore("Themenbereich",
-		rec => 
-		({
-			"fields":
-			{
-				"Themenbereich": rec.fields.Themenbereich,
-				"Thema": getThemaId(rec),
-				"Fachbereich": getFachbereichId(rec),
-				"Fach": getFachId(rec)
-			}
-		}), 
-	restore("Dysfunktionen",
-		rec => 
-		({
-			"fields":
-			{
-				"Dysfunktionen": rec.fields.Dysfunktionen,
-				"Themenbereich": getThemenbereichId(rec),
-				"Thema": getThemaId(rec),
-				"Fachbereich": getFachbereichId(rec),
-				"Fach": getFachId(rec)
-			}
-		}), 
-	restore("Pathologie",
-		rec => 
-		({
-			"fields":
-			{
-				"Pathologie": rec.fields.Pathologie,
-				"Dysfunktionen": getDysfunktionenId(rec),
-				"Themenbereich": getThemenbereichId(rec),
-				"Thema": getThemaId(rec),
-				"Fachbereich": getFachbereichId(rec),
-				"Fach": getFachId(rec)
-			}
-		}), 
-	restore("Dozent", 
-		rec => 
-		({
-			"fields":
-			{
-				"Dozent": rec.fields.Dozent
-			}
-		}),
-	restore("Jahrgang", 
-		rec => 
-		({
-			"fields":
-			{
-				"Jahrgang": rec.fields.Jahrgang
-			}
-		}),
-	restore("Klassenbuch",
-		rec => 
-		({
-			"fields":
-			{
-				"Name": rec.fields.Name,
-				"Dozent": getDozentId(rec),
-				"Assistent": rec.fields.Assistent,
-				"Jahrgang": getJahrgangId(rec),
-				"Datum": rec.fields.Datum,
-				"Notes": rec.fields.Notes,
-				"Fach": getFachId(rec),
-				"Fachbereich": getFachbereichId(rec),
-				"Thema": getThemaId(rec),
-				"Themenbereich": getThemenbereichId(rec),
-				"Dysfunktionen": getDysfunktionenId(rec),
-				"Pathologie": getPathologieId(rec),
-				"Stunden": rec.fields.Stunden
-			}
-		}), 
-	)))))))))();
-})();
+restoreAll()
